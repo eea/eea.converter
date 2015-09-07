be31150b6c979bb7b9a3e271bec1bcec1ea94791
Alin Voinea <alin@eaudeweb.ro>
2015-09-07 19:27:47 +0300

    Refs #28522 - Moved PDF metadata parser from eea.reports

--- eea/converter/pdf/parser.py
+++ eea/converter/pdf/parser.py
@@ -1,18 +1,13 @@
 """ Metadata parser
 """
-import os
-import re
-import tempfile
 import logging
-import StringIO
-from subprocess import Popen, PIPE, STDOUT
-from types import InstanceType, StringType, UnicodeType, FileType
+from PyPDF2 import PdfFileReader
 from zope import interface
 from zope.component.hooks import getSite
 from Products.CMFCore.utils import getToolByName
 from eea.converter.interfaces import IPDFParser
 
-logger = logging.getLogger('eea.reports.pdf')
+logger = logging.getLogger('eea.converter')
 
 class PDFParser(object):
     """ parses metadata from pdf files """
@@ -77,55 +72,22 @@ class PDFParser(object):
 
         return metadata
 
-    def _parse(self, pdf, password=''):
+    def _parse(self, pdf, password='', **kwargs):
         """ parses the given pdf file and returns a mapping of attributes """
         metadata = self._parsepdf(pdf, password)
         if not metadata:
             metadata = {}
 
-        # Get plain/text metadata
-        tmp_pdf = tempfile.mkstemp(suffix='.pdf')
-        fd = open(tmp_pdf[1], 'w')
-        fd.write(pdf)
-        fd.close()
-        statement = 'pdfinfo '
+        opdf = PdfFileReader(pdf)
         if password:
-            statement += '-opw %s ' % password
-        statement += tmp_pdf[1]
-        logger.debug('pdfinfo commandline: %s', statement)
-        process = Popen(statement, shell=True,
-                        stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-        result = process.stdout.read()
+            opdf.decrypt(password)
 
-        # cleanup the tempfile
-        os.remove(tmp_pdf[1])
+        info = opdf.getDocumentInfo()
 
-        # check for errors or encryption
-        if result.startswith(
-            'Error: No paper information available - using defaults'):
-            # Irritating error if libpaper is not configured correctly.
-            # For our case this is irrelevant
-            pass
-        elif result.startswith('Error'):
-            error = result.split('\n')[0]
-            logger.error("Error in pdfinfo conversion: %s", error)
-            return metadata
-        elif 'command not found' in result:
-            return metadata
+        new_metadata = dict(
+            (key.strip('/').lower(), val) for key, val in info.items()
+        )
 
-        crypt_patt = re.compile('Encrypted:.*?copy:no', re.I)
-        mobj = crypt_patt.search(result, 1)
-        if mobj is not None:
-            error = "Error: PDF is encrypted"
-            logger.error(error)
-            return metadata
-
-        result = result.replace('  ', ' ').replace('\r\n', '\n')
-        res_list = result.splitlines()
-        new_metadata = [[r.strip() for r in x.split(':', 1)]
-                        for x in res_list]
-        new_metadata = dict((x[0].lower(), x[1]) for x in new_metadata
-                            if len(x) == 2)
         metadata.update(new_metadata)
         #
         # Fix some metadata
@@ -133,164 +95,54 @@ class PDFParser(object):
         metadata = self._fix_metadata(metadata)
         return metadata
 
-    def _parsepdf(self, pdf, owner_password='', user_password=''):
+    def _parsepdf(self, pdf, password='', **kwargs):
         """ parses the given pdf file and returns a mapping of attributes """
 
         # This will store the parsed metadata
         META_MAP = {}
 
-        statement = "pdfinfo -meta"
-        if owner_password != "":
-            statement += ' -opw ' + owner_password
-        if user_password != "":
-            statement += ' -upw ' + user_password
+        opdf = PdfFileReader(pdf)
 
-        # pdfinfo needs to work on a file. Write the file and start pdfinfo
-        tmp_pdf = tempfile.mkstemp(suffix='.pdf')
-        fd = open(tmp_pdf[1], 'w')
-        if isinstance(pdf, InstanceType) and pdf.__class__ == StringIO.StringIO:
-            fd.write(pdf.getvalue())
-        elif isinstance(pdf, StringType) or isinstance(pdf, UnicodeType):
-            fd.write(pdf)
-        elif isinstance(pdf, FileType):
-            fd.write(str(pdf))
-        else:
-            raise ValueError, 'Cannot determine type of pdf variable'
-        fd.close()
+        if password != "":
+            opdf.decrypt(password)
 
-        statement += ' '+tmp_pdf[1]
-        logger.debug('pdfinfo commandline: %s', statement)
-        process = Popen(statement, shell=True,
-                        stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        metadata = opdf.getXmpMetadata()
 
-        # get the result
-        result = process.stdout.read()
-        logger.debug('metadata extracted by pdfinfo :\n---------\n%s ', result)
+        if getattr(metadata, 'pdf_keywords', None):
+            META_MAP['keywords'] = metadata.pdf_keywords
+        if getattr(metadata, 'dc_language', None):
+            META_MAP['language'] = metadata.dc_language
+        if getattr(metadata, 'dc_identifier', None):
+            META_MAP['uuid'] = metadata.dc_identifier
+        if getattr(metadata, 'xmpmm_documentId', None):
+            META_MAP['uuid'] = metadata.xmpmm_documentId
+        if getattr(metadata, 'xmpmm_instanceId', None):
+            META_MAP['uuid'] = metadata.xmpmm_instanceId
+        if getattr(metadata, 'xmp_createDate', None):
+            META_MAP['creationdate'] = metadata.xmp_createDate
+        if getattr(metadata, 'xmp_modifyDate', None):
+            META_MAP['modificationdate'] = metadata.xmp_modifyDate
+        if getattr(metadata, 'xmp_metadataDate', None):
+            META_MAP['metadatadate'] = metadata.xmp_metadataDate
+        if getattr(metadata, 'dc_rights', None):
+            META_MAP['rights webstatement'] = metadata.dc_rights
+        if getattr(metadata, 'pdf_producer', None):
+            META_MAP['producer'] = metadata.pdf_producer
+        if getattr(metadata, 'xmp_creatorTool', None):
+            META_MAP['creatortool'] = metadata.xmp_creatorTool
+        if getattr(metadata, 'dc_title', None):
+            META_MAP['title'] = metadata.dc_title
+        if getattr(metadata, 'dc_description', None):
+            META_MAP['description'] = metadata.dc_description
+        if getattr(metadata, 'dc_rights', None):
+            META_MAP['rights'] = metadata.dc_rights
+        if getattr(metadata, 'dc_format', None):
+            META_MAP['format'] = metadata.dc_format
+        if getattr(metadata, 'dc_creator', None):
+            META_MAP['creator'] = metadata.dc_creator
 
-        # cleanup the tempfile
-        os.remove(tmp_pdf[1])
-
-        # check for errors or encryption
-        if result.startswith(
-            'Error: No paper information available - using defaults'):
-            # Irritating error if libpaper is not configured correctly.
-            # For our case this is irrelevant
-            pass
-        elif result.startswith('Error'):
-            error = result.split('\n')[0]
-            logger.error("Error in pdfinfo conversion: %s", error)
-            return False
-
-        crypt_patt = re.compile('Encrypted:.*?copy:no', re.I)
-        mobj = crypt_patt.search(result, 1)
-        if mobj is not None:
-            error = "Error: PDF is encrypted"
-            logger.error(error)
-            return False
-
-        # Everything seems fine, parse the metadata
-        # Caution: do not use the metalist, it's not unicode!
-        # Note that pdfinfo returns a ini style list and an xml version.
-        METADATA = result.split('Metadata:')
-        if len(METADATA) > 1:
-            metaxml = METADATA[1]
-        else:
-            metaxml = ''
-
-        # Get metadata out of the xml-part
-        # If would be a good idea to make this generic
-        # It even would be smart to use an xml parser here.
-        patt_list = []
-        patt_list.append(
-            ('Keywords', "<pdf:Keywords>(.*?)</pdf:Keywords>"))
-        patt_list.append(
-            ('Keywords', "pdf:Keywords='(.*?)'"))
-        patt_list.append(
-            ('Language', "<pdf:Language>(.*?)</pdf:Language>"))
-        patt_list.append(
-            ('Language', "pdf:Language='(.*?)'"))
-        patt_list.append(
-            ('UUID', "xapMM:DocumentID='uuid:(.*?)'"))
-        patt_list.append(
-            ('UUID', 'rdf:about="uuid:(.*?)"'))
-        patt_list.append(
-            ('CreationDate', "xap:CreateDate='(.*?)'"))
-        patt_list.append(
-            ('CreationDate', "<xap:CreateDate>(.*?)</xap:CreateDate>"))
-        patt_list.append(
-            ('ModificationDate', "xap:ModifyDate='(.*?)'"))
-        patt_list.append(
-            ('ModificationDate', "<xap:ModifyDate>(.*?)</xap:ModifyDate>"))
-        patt_list.append(
-            ('MetadataDate', "xap:MetadataDate='(.*?)'"))
-        patt_list.append(
-            ('MetadataDate', "<xap:MetadataDate>(.*?)</xap:MetadataDate>"))
-        patt_list.append(
-            ('Rights Webstatement',
-             "<xapRights:WebStatement>(.*?)</xapRights:WebStatement>"))
-        patt_list.append(
-            ('Producer', "<pdf:Producer>(.*?)</pdf:Producer>"))
-        patt_list.append(
-            ('CreatorTool', "<xap:CreatorTool>(.*?)</xap:CreatorTool>"))
-        patt_list.append(
-            ('Title', "<dc:title>(.*?)</dc:title>"))
-        patt_list.append(
-            ('Description', "<dc:description>(.*?)</dc:description>"))
-        patt_list.append(
-            ('Rights', "<dc:rights>(.*?)</dc:rights>"))
-        patt_list.append(
-            ('Format', "<dc:format>(.*?)</dc:format>"))
-        patt_list.append(
-            ('Creator', "<dc:creator>(.*?)</dc:creator>"))
-        patt_list.append(
-            ('OPOCE', "pdfx:OPOCE='(.*?)'"))
-        patt_list.append(
-            ('OPOCE', "<pdfx:OPOCE>(.*?)</pdfx:OPOCE>"))
-
-        for patt in patt_list:
-            pobj = re.compile(patt[1], re.I | re.S)
-            mobj = pobj.search(metaxml, 1)
-            if mobj is not None:
-                value = re.sub('<.*?>', '', mobj.group(1)).strip()
-                # acrobat separates keywords with a semicolon.
-                # There is no datatyping
-                # so we assume it is a list if a semicolon appears.
-                if ";" in value:
-                    kw = value.split(";")
-                    value = tuple([x.strip() for x in kw])
-
-                META_MAP[patt[0].strip().lower()] = value
-            else:
-                logger.debug("No matches for %s", str(patt[1]))
-
-
-        # Get the user-defined meta-data
-        add_patt = re.compile("pdfx:(.*?)='(.*?)'", re.I|re.S)
-
-        for name, value in add_patt.findall(metaxml):
-            # acrobat separates keywords with a semicolon.
-            # There is no datatyping
-            # so we assume it is a list if a semicolon appears.
-            if ";" in value:
-                kw = value.split(";")
-                value = tuple([y.strip() for y in kw])
-            META_MAP[name.strip().lower()] = value
-
-        # And another format
-        add_patt = re.compile("pdfx:(.*?)>(.*?)</pdfx:", re.I|re.S)
-        for name, value in add_patt.findall(metaxml):
-            # acrobat separates keywords with a semicolon.
-            # There is no datatyping
-            # so we assume it is a list if a semicolon appears.
-            if ";" in value:
-                kw = value.split(";")
-                value = tuple([z.strip() for z in kw])
-            META_MAP[name.strip().lower()] = value
-
-
-        # If the language is given in the filename extension,
-        # we consider that as
-        # most explicit
+        if getattr(metadata, 'custom_properties', None):
+            META_MAP.update(metadata.custom_properties)
 
         l = self._guessLanguage(pdf)
         if l and not META_MAP.has_key('language'):
