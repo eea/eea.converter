18e78384d847f482077864dd71c2671319a0f741
Alin Voinea <alin@eaudeweb.ro>
2015-12-11 10:51:47 +0200

    Refs #31324 - Fix wkhtmltox dependency for CentOS

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name=NAME,
               ],
           'yum': [
               'xorg-x11-server-Xvfb',
-              'wkhtmltopdf',
+              'wkhtmltox',
               'ImageMagick'
               ],
           'apt': [
