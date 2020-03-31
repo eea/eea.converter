827e52c30ff07bd80167107f50f5839586d8a596
David Ichim <ichim.david@gmail.com>
2020-03-31 19:18:51 +0300

    * Change: use title from parsed metadata instead of encoding it:
    - we have tests in eea.reports which fail as the test expected
      the title to be unicode and the changes in this ticket encoded
      the title
      [ichimd-david refs #116365]

--- eea/converter/pdf/parser.py
+++ eea/converter/pdf/parser.py
@@ -93,12 +93,12 @@ class PDFParser(object):
             (key.strip('/').lower(), val) for key, val in info.items()
         )
 
-        # #116365 encode title from pdf parsing instead of the data coming
+        # #116365 use title from pdf parsing instead of the data coming
         # from pypdf2 getDocumentInfo method as that method will wrongly
         # encode an mdash found in the title
         parsed_title = metadata.get('title', {}).get('x-default')
         if parsed_title:
-            new_metadata['title'] = parsed_title.encode('utf-8')
+            new_metadata['title'] = parsed_title
 
         metadata.update(new_metadata)
         #
